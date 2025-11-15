59537f7ea93b0e35bc2d584407fad3aaaa0fd792
Abdullah <143931418+TypeAbdullah@users.noreply.github.com>
2025-11-15 18:14:14 +0600

    Update config.py

--- config.py
+++ config.py
@@ -14,8 +14,8 @@ LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID", 0))
 # ----------------
 
 # --- PICTURES ---
-START_PIC_URL = "https://graph.org/file/ab1c4882db6a02add069e-2d59575f154d39acda.jpg"
-PROFILE_PIC_URL = "https://graph.org/file/ab1c4882db6a02add069e-2d59575f154d39acda.jpg"
+START_PIC_URL = "https://graph.org/file/34891a7555dcdbf29068e-402ea87b8c7fdc8f23.jpg"
+PROFILE_PIC_URL = "https://graph.org/file/37486760ded5cd4097437-51cf00c61fe05cec37.jpg"
 
 # --- TEXTS ---
 OWNER_ID = 7099729191 
